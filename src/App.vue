004de49be59e592f62d05c3f841b31b1302ffeb1
GabrieleSalati <gabriele.salati5@gmail.com>
2023-05-16 18:54:54 +0200

    Restaurants show functional

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+<script>
+
+import axios from "axios";
+import RestaurantShow from './components/partials/UserComponents/RestaurantShow.vue';
+
+export default {
+  data() {
+    return {
+      restaurants: []
+    }
+  },
+
+  components: {
+    RestaurantShow,
+  },
+
+
+  methods: {
+    fetchRestaurants() {
+      axios.get('http://127.0.0.1:8000/api/restaurants').then((response) => { this.restaurants = response.data; })
+    }
+  },
+
+  created() {
+    this.fetchRestaurants();
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <RestaurantShow :restaurants="restaurants" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/partials/UserComponents/RestaurantShow.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+
+    props: {
+        restaurants: Array,
+    },
+
+};
+</script>
+
+<template>
+    <div class="card" v-for="restaurant in restaurants">
+        <div class="card-body">
+            <h5 class="card-title">{{ restaurant.restaurant_name }}</h5>
+            <p class="card-text">{{ restaurant.p_iva }}</p>
+            <p class="card-text">{{ restaurant.address }}</p>
+            <!-- <img :src="project.image" class="card-img-top" alt="Picture"> -->
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
