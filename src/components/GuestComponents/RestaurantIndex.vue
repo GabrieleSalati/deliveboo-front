ed4a16a8ce5e4b86bf289961b4e44711dc833a72
GabrieleSalati <gabriele.salati5@gmail.com>
2023-05-18 16:40:06 +0200

    Restaurants style modified

--- src/components/GuestComponents/RestaurantIndex.vue
+++ src/components/GuestComponents/RestaurantIndex.vue
@@ -34,16 +34,16 @@ export default {
 
 <template>
     <div class="container card-list">
-        <div class="row">
-            <div class="card col-6" v-for="restaurant in restaurants">
-                <div class="card-body">
+        <div class="row justify-content-center">
+            <div class="card col-4 g-4 p-0 mx-5" v-for="restaurant in restaurants">
+                <img :src="restaurant.picture" class="card-img-top" alt="Picture">
+                <div class="card-body p-4">
                     <h5 class="card-title">{{ restaurant.restaurant_name }}</h5>
                     <p class="card-text">{{ restaurant.p_iva }}</p>
                     <p class="card-text">{{ restaurant.address }}</p>
                     <div v-for="category in restaurant.categories">
                         <p class="card-text">{{ category.label }}</p>
                     </div>
-                    <img :src="restaurant.picture" class="card-img-top" alt="Picture">
                 </div>
             </div>
         </div>
@@ -52,7 +52,21 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+img {
+    width: 100%;
+    max-height: 200px;
+}
+
+.card {
+    border: none;
+}
+
+.card-body {
+    background-color: aquamarine;
+}
+
 .card-list {
     margin-top: 10vh;
+    margin-bottom: 10vh;
 }
 </style>
