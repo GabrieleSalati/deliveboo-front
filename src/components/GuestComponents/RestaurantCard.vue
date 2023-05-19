ac54c5b3c68e9ac7eb5bd065b1ba1b5598f3b151
Thomas-Basadonne <thommybasa@gmail.com>
2023-05-19 13:08:37 +0200

    reastaurantCard Placeholder

--- src/components/GuestComponents/RestaurantCard.vue
+++ src/components/GuestComponents/RestaurantCard.vue
@@ -11,20 +11,19 @@ export default {
 </script>
 
 <template>
-  <!-- <div>
-    <h1>{{ title }}</h1>
-    <div class="card" v-if="restaurant">
+  <div>
+    <div class="card">
       <div class="card-body">
-        <h5 class="card-title">{{ restaurant.restaurant_name }}</h5>
+        <h5 class="card-title"></h5>
         <h6 class="card-subtitle mb-2 text-muted">{{ restaurant.address }}</h6>
         <p class="card-text">{{ restaurant.description }}</p>
         <p class="card-text">P.IVA: {{ restaurant.p_iva }}</p>
       </div>
     </div>
-  </div> -->
+  </div>
 
   <!-- PLACEHOLDER CARD DA MODIFY -->
-  <div class="container my-5">
+  <!-- <div class="container my-5">
     <div class="card mb-3">
       <img
         src="../../assets/jumbo-.svg"
@@ -40,40 +39,15 @@ export default {
           </p>
           <p>Category: Pizza, Pizza, Pizza</p>
         </div>
-        <div class="menu">
-          <div class="list-group my-5" id="myList" role="tablist">
-            <li class="list-group-item text-center fw-bold fs-2">Menu</li>
-            <div
-              class="list-group-item list-group-item-action d-flex justify-content-center"
-            >
-              <img
-                src="../../assets/jumbo-.svg"
-                class="card-img-top img-fluid"
-                alt=""
-              />
-              <div class="text-start px-4 py-3">
-                <h4>Nome piatto</h4>
-                <p>
-                  Descrizione: Lorem, ipsum dolor sit amet consectetur
-                  adipisicing elit. Adipisci porro soluta laborum, at fugiat
-                  architecto quidem nostrum illum eligendi.
-                </p>
-                <p>Prezzo:12</p>
-                <div>
-                  <i class="bi bi-cart-plus"></i>
-                  <i class="bi bi-cart-dash"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        
+        QUI CI VA IL MENU
 
         <p class="card-text">
           <small class="text-body-secondary">P. IVA: 11111111111</small>
         </p>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <style lang="scss" scoped>
