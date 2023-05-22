9a98427c32f57049010e9fc59fe2105f39d44a89
Thomas-Basadonne <thommybasa@gmail.com>
2023-05-23 01:36:35 +0200

    init extra page

---
+++ src/components/ExtraComponents/AboutUs.vue
@@ -0,0 +1,20 @@
+<script>
+import AppHeader from "../_partials/AppHeader.vue";
+import AppFooter from "../_partials/AppFooter.vue";
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
+
+<template><AppHeader />, <AppFooter />,</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/ExtraComponents/InfoPrivacy.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from "../_partials/AppHeader.vue";
+import AppFooter from "../_partials/AppFooter.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
+
+<template><AppHeader />, <AppFooter />,</template>
+
+<style lang="scss" scoped></style>
