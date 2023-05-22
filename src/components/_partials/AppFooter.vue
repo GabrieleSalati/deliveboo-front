6565f6d926c60e5b90931dba2dbd5dc1daf429bb
Thomas-Basadonne <thommybasa@gmail.com>
2023-05-22 19:24:47 +0200

    fix footer + fix homepage + delete usless file

--- src/components/_partials/AppFooter.vue
+++ src/components/_partials/AppFooter.vue
@@ -1,6 +1,4 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
-
 export default {
   data() {
     return {
@@ -10,22 +8,22 @@ export default {
 
   methods: {
     redirectToPage() {
-      window.location.href = "http://127.0.0.1:8000/login";
+      window.location.href = "http://127.0.0.1:8000/";
     },
-  }
+  },
 };
 </script>
 
 <template>
   <!-- Footer -->
-  <footer class="text-center text-lg-start text-light">
+  <footer class="text-sm-center text-lg-start text-light">
     <!-- Section: Links  -->
     <section>
-      <div class="container text-center text-md-start pt-5">
+      <div class="container text-center pt-5">
         <!-- Grid row -->
-        <div class="row mt-3">
+        <div class="row mt-3 d-flex justify-content-between custom-row">
           <!-- Grid column -->
-          <div class="block col-md-3 col-lg-4 col-xl-3 mx-1 mb-4 pt-3 rounded">
+          <div class="bg-block col-lg-3 col-md-6 col-sm-12 mb-4 pt-3 rounded">
             <!-- Content -->
             <h6 class="text-uppercase fw-bold mb-4">DeliveBoo</h6>
             <p>
@@ -34,35 +32,37 @@ export default {
               riceverlo comodamente a casa tua. Ordina subito!
             </p>
           </div>
-          <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="block col-md-3 col-lg-3 col-xl-2 mx-1 mb-4 pt-3 rounded">
+          <div class="bg-block col-lg-3 col-md-6 col-sm-12 mb-4 pt-3 rounded">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4 text-center">Menu Rapido</h6>
-            <p class="text-center">
+            <p>
               <router-link :to="{ name: 'home' }" class="nav-link text-light">
                 Home
               </router-link>
             </p>
-            <p class="text-center">
-              <router-link :to="{ name: 'restaurants' }" class="nav-link text-light">
+            <p>
+              <router-link
+                :to="{ name: 'restaurants' }"
+                class="nav-link text-light"
+              >
                 Ristoranti
               </router-link>
             </p>
-            <p>
+            <div class="my-3 d-flex justify-content-center">
               <button @click="redirectToPage" class="nav-link text-light">
                 Sei un ristoratore?
               </button>
-            </p>
-            <p class="text-center">
+            </div>
+
+            <p>
               <a href="#!" class="text-reset">Chi Siamo</a>
             </p>
           </div>
-          <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="block col-md-2 col-lg-3 col-xl-2 mx-1 mb-4 pt-3 rounded">
+          <div class="bg-block col-lg-3 col-md-6 col-sm-12 mb-4 pt-3 rounded">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4 text-center">Note Legali</h6>
             <p>
@@ -75,41 +75,50 @@ export default {
               <a href="#!" class="text-reset">Cookies</a>
             </p>
           </div>
-          <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="block col-md-3 col-lg-4 col-xl-3 mx-1  mb-4  pt-3 rounded">
+          <div class="bg-block col-lg-3 col-md-6 col-sm-12 mb-4 pt-3 rounded">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">I nostri Contatti</h6>
             <p>
-              <a href="https://www.linkedin.com/in/thomasbasadonne" class="text-reset">Thomas Basadonne</a>
+              <a
+                href="https://www.linkedin.com/in/thomasbasadonne"
+                class="text-reset"
+                >Thomas Basadonne</a
+              >
             </p>
             <p>
-              <a href="https://www.linkedin.com/in/sofia-curreri-38b281276" class="text-reset">Sofia Curreri</a>
+              <a
+                href="https://www.linkedin.com/in/sofia-curreri-38b281276"
+                class="text-reset"
+                >Sofia Curreri</a
+              >
             </p>
             <p>
-              <a href="https://www.linkedin.com/in/hadolph-mercogliano-00016b275/" class="text-reset">Hadolph
-                Mercogliano</a>
+              <a
+                href="https://www.linkedin.com/in/hadolph-mercogliano-00016b275/"
+                class="text-reset"
+                >Hadolph Mercogliano</a
+              >
             </p>
             <p>
-              <a href="https:/www.linkedin.com/in/gabriele-salati-910091275" class="text-reset">Gabriele Salati</a>
+              <a
+                href="https:/www.linkedin.com/in/gabriele-salati-910091275"
+                class="text-reset"
+                >Gabriele Salati</a
+              >
             </p>
           </div>
-          <!-- Grid column -->
         </div>
-        <!-- Grid row -->
       </div>
     </section>
-    <!-- Section: Links  -->
 
     <!-- Copyright -->
     <div class="text-center p-4">
       © 2023 Copyright:
       <a class="text-reset fw-bold" href="#">DeliveBoo.com</a>
     </div>
-    <!-- Copyright -->
   </footer>
-  <!-- Footer -->
 </template>
 
 <style lang="scss" scoped>
@@ -117,7 +126,7 @@ footer {
   background-color: #2e3333;
 }
 
-.block {
+.bg-block {
   background-color: #484b4b;
 }
 
@@ -125,7 +134,19 @@ a {
   text-decoration: none;
 }
 
-button {
-  width: 100%;
+@media (min-width: 768px) and (max-width: 991.98px) {
+  .bg-block {
+    max-width: 40%;
+  }
+  .custom-row {
+    justify-content: center !important;
+    gap: 2rem;
+  }
+}
+
+@media (min-width: 992px) {
+  .bg-block {
+    max-width: 22%;
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "../components/GuestComponents/HomePage.vue";
 import RestaurantIndex from "../components/GuestComponents/RestaurantIndex.vue";
-import Menu from "../components/GuestComponents/Menu.vue";
 import RestaurantCard from "../components/GuestComponents/RestaurantCard.vue";
 
 const router = createRouter({
@@ -25,12 +24,6 @@ const router = createRouter({
       path: "/restaurants/:id",
       name: "restaurant-card",
       component: RestaurantCard,
-    },
-
-    {
-      path: "/menu",
-      name: "menu",
-      component: Menu,
     },
   ],
 });
