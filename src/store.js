4cbb997a603e7d4476066d47c6476469a8bb5136
SofiaCurreri <sofiacurreri@gmail.com>
2023-05-23 19:12:06 +0200

    nuovo file store.js, commentato per ora

---
+++ src/store.js
@@ -0,0 +1,17 @@
+// import { reactive } from "vue";
+
+// export const store = reactive({
+//   totalCartDishesQuantity() {
+//     let cartQuantity = 0;
+//     for (let i = 0; i < this.cartItems.length; i++) {
+//       const cartItem = this.cartItems[i];
+//       cartQuantity += cartItem.quantity;
+//     }
+//     return cartQuantity;
+//   },
+
+//   //????
+//   setTotalCartDishesnumber(value) {
+//     store.totalCartDishesQuantity() = value;
+//   },
+// });
