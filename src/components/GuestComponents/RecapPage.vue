84ed1d8c7055051de15ebdb61c9c2799276832a0
GabrieleSalati <gabriele.salati5@gmail.com>
2023-05-23 11:29:33 +0200

    Form work in progress

--- src/components/GuestComponents/RecapPage.vue
+++ src/components/GuestComponents/RecapPage.vue
@@ -172,6 +172,17 @@ export default {
 <template>
     <h1>{{ title }}</h1>
     <Cart :cartItems="cartItems" />
+    <h2>Contatta il ristoratore:</h2>
+    <div class="container">
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
