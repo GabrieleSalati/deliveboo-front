856a5f511999115245f30120e1f38cd9850ebd3c
GabrieleSalati <gabriele.salati5@gmail.com>
2023-05-23 11:04:55 +0200

    Cart functional

--- src/components/GuestComponents/RecapPage.vue
+++ src/components/GuestComponents/RecapPage.vue
@@ -1,22 +1,177 @@
 <script>
 import Cart from "../GuestComponents/Cart.vue"
+import localStorageMixin from "../../localStorageMixin.js";
+import axios from "axios";
 
 export default {
+    mixins: [localStorageMixin],
+
     data() {
         return {
-            title: "Ordina e paga!"
+            title: "Ordina e paga!",
+            restaurant: {}, //ristorante
+            cartItems: [], //carrello
+            dishesList: [], //products
+            // index: null,
+            totalCartDishesnumber: 0,
+            key: "carrello",
         }
     },
 
     components: {
         Cart,
     },
+    computed: {
+        // totalCartDishes2() {
+        //   let totalCartDishesnumber = 0;
+        //   for (let i = 0; i < this.cartItems.length; i++) {
+        //     const quantityEl = this.cartItems[i].quantity;
+        //     totalCartDishesnumber += quantityEl;
+        //   }
+        //   return totalCartDishesnumber;
+        // },
+        totalCartDishesnumber() {
+            let sumQuantity = 0;
+            for (let i = 0; i < this.cartItems.length; i++) {
+                const quantityEl = this.cartItems[i].quantity;
+                sumQuantity += quantityEl;
+            }
+            return (this.totalCartDishesnumber = sumQuantity);
+        },
+    },
+    created() {
+        axios.get(`http://127.0.0.1:8000/api/restaurants/${this.$route.params.id}`).then((response) => {
+            this.restaurant = response.data[0];
+            this.dishesList = this.restaurant.dishes;
+            console.log(this.restaurant);
+            console.log(this.dishesList);
+        });
+        // localStorage.clear();
+        this.init();
+    },
+
+    methods: {
+        init() {
+            this.cartItems = this.getFromLocalStorage(this.key);
+            console.log("log di cartitems nell' init", this.cartItems);
+            if (!this.cartItems) {
+                this.cartItems = [];
+            }
+        },
+
+        // incrementCounter(dishId) {
+        //   // TODO portare come parametro il piatto intero e confrontare gli id
+        //   const cartItem = this.cartItems.find((item) => item.id === dishId);
+        //   if (cartItem) {
+        //     cartItem.quantity++;
+        //   } else {
+        //     let arr = this.dishesList.filter((dish) => {
+        //       if (dish.id === dishId) {
+        //         console.log("oggeto trovato dal filter", dish);
+        //         return true;
+        //       }
+        //     });
+        //     if (arr && arr[0]) {
+        //       let obj = {
+        //         id: arr[0].id,
+        //         restaurant_id: arr[0].restaurant_id,
+        //         name: arr[0].name,
+        //         quantity: 1,
+        //         price: arr[0].price,
+        //         picture: arr[0].picture,
+        //       };
+        //       this.cartItems.push(obj);
+        //       this.sync(this.key, this.cartItems);
+        //       // this.cartItems.push({
+        //       //   dishId: dishId,
+        //       //   quantity: 1,
+
+        //       // });
+        //     } else {
+        //       //product id does not exist in products data
+        //       console.error("Invalid Product");
+        //     }
+        //     console.log(this.cartItems);
+        //   }
+        // },
+        incrementCounter(dish) {
+            const cartItem = this.cartItems.find((item) => item.id === dish.id);
+            if (cartItem) {
+                cartItem.quantity++;
+            } else {
+                let obj = {
+                    id: dish.id,
+                    restaurant_id: dish.restaurant_id,
+                    name: dish.name,
+                    quantity: 1,
+                    price: dish.price,
+                    picture: dish.picture,
+                };
+                this.cartItems.push(obj);
+            }
+            console.log(this.cartItems);
+            this.sync(this.key, this.cartItems);
+        },
+        // incrementCounter(dishId) {
+        //   // TODO portare come parametro il piatto intero e confrontare gli id
+        //   const cartItem = this.cartItems.find((item) => item.dishId === dishId);
+        //   if (cartItem) {
+        //     cartItem.quantity++;
+        //   } else {
+        //     this.cartItems.push({
+        //       dishId: dishId,
+        //       quantity: 1,
+        //     });
+        //   }
+        //   console.log(this.cartItems);
+        // },
+
+        decrementCounter(dishId) {
+            const cartItem = this.cartItems.find((item) => item.id === dishId);
+            if (cartItem && cartItem.quantity > 0) {
+                cartItem.quantity--;
+            }
+            if (cartItem && cartItem.quantity == 0) {
+                // const index = this.cartItems.indexOf(cartItem);
+                this.cartItems.splice(this.getIndexItem(dishId), 1);
+            }
+            console.log(this.cartItems);
+            this.sync(this.key, this.cartItems);
+        },
+
+        getCartItemQuantity(dishId) {
+            const cartItem = this.cartItems.find((item) => item.id === dishId);
+            return cartItem ? cartItem.quantity : 0;
+        },
+
+        getIndexItem(dishId) {
+            const cartItem = this.cartItems.find((item) => item.id == dishId);
+            const index = this.cartItems.indexOf(cartItem);
+            console.log("index", index);
+            return index;
+        },
+
+        //   saveToCart(dishId) {
+        //     // this.getIndexItem(dishId);
+        //     // const cartItem = this.cartItems.find((item) => item.id == dishId);
+        //     this.sync(this.key, this.cartItems);
+        // },
+        totalCartDishes() {
+            let sumQuantity = 0;
+            for (let i = 0; i < this.cartItems.length; i++) {
+                const quantityEl = this.cartItems[i].quantity;
+                sumQuantity += quantityEl;
+            }
+            return sumQuantity;
+        },
+    },
 };
+
 </script>
 
 <template>
     <h1>{{ title }}</h1>
-    <Cart />
+    <Cart :cartItems="cartItems" />
 </template>
 
 <style lang="scss" scoped></style>
