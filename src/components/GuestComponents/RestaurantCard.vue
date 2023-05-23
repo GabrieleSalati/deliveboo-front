4729246067624d78118dd1507a8decdce195869a
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-05-23 12:03:26 +0200

    added control for restaurant_id

--- src/components/GuestComponents/RestaurantCard.vue
+++ src/components/GuestComponents/RestaurantCard.vue
@@ -21,14 +21,6 @@ export default {
     Cart,
   },
   computed: {
-    // totalCartDishes2() {
-    //   let totalCartDishesnumber = 0;
-    //   for (let i = 0; i < this.cartItems.length; i++) {
-    //     const quantityEl = this.cartItems[i].quantity;
-    //     totalCartDishesnumber += quantityEl;
-    //   }
-    //   return totalCartDishesnumber;
-    // },
     totalCartDishesnumber() {
       let sumQuantity = 0;
       for (let i = 0; i < this.cartItems.length; i++) {
@@ -39,14 +31,12 @@ export default {
     },
   },
   created() {
-    axios
-      .get(`http://127.0.0.1:8000/api/restaurants/${this.$route.params.id}`)
-      .then((response) => {
-        this.restaurant = response.data[0];
-        this.dishesList = this.restaurant.dishes;
-        console.log(this.restaurant);
-        console.log(this.dishesList);
-      });
+    axios.get(`http://127.0.0.1:8000/api/restaurants/${this.$route.params.id}`).then((response) => {
+      this.restaurant = response.data[0];
+      this.dishesList = this.restaurant.dishes;
+      console.log(this.restaurant);
+      console.log(this.dishesList);
+    });
     // localStorage.clear();
     this.init();
   },
@@ -63,12 +53,15 @@ export default {
     incrementCounter(dish) {
       const cartItem = this.cartItems.find((item) => item.id === dish.id);
 
-      console.log("dish.restaurant_id:", dish.restaurant_id);
-      console.log("this.restaurant.id:", this.restaurant.id);
+      // console.log("dish.restaurant_id:", dish.restaurant_id);
+      // console.log("this.restaurant.id:", this.restaurant.id);
 
       if (cartItem && cartItem.restaurant_id === dish.restaurant_id) {
         cartItem.quantity++;
-      } else if (dish.restaurant_id === this.restaurant.id) {
+      } else if (
+        this.cartItems.length == 0 ||
+        this.cartItems[0].restaurant_id == dish.restaurant_id
+      ) {
         let obj = {
           id: dish.id,
           restaurant_id: dish.restaurant_id,
@@ -160,29 +153,20 @@ export default {
       <div class="card-body">
         <div class="info-restaurant text-center">
           <h5 class="card-title fs-1">{{ restaurant.restaurant_name }}</h5>
-          <p class="card-text">
-            <i class="bi bi-geo-alt me-1"></i>{{ restaurant.address }}
-          </p>
+          <p class="card-text"><i class="bi bi-geo-alt me-1"></i>{{ restaurant.address }}</p>
           <p>{{ restaurant.category }}</p>
         </div>
 
         <div class="menu">
           <div class="list-group d-flex my-5" id="myList" role="tablist">
-            <div class="menu-title text-center fw-bold fs-2 my-4 rounded-top">
-              Menu
-            </div>
+            <div class="menu-title text-center fw-bold fs-2 my-4 rounded-top">Menu</div>
             <div
               v-for="dish in restaurant.dishes"
               :key="dish.id"
-              class="list-group-item row list-group-item-action d-flex justify-content-start align-self-center"
-            >
+              class="list-group-item row list-group-item-action d-flex justify-content-start align-self-center">
               <!-- MODIFICA FLEX BREAK POINT -->
               <div class="col-4">
-                <img
-                  :src="dish.picture"
-                  class="dish-picture img-fluid"
-                  alt="piatto"
-                />
+                <img :src="dish.picture" class="dish-picture img-fluid" alt="piatto" />
               </div>
               <div class="col-7">
                 <div class="text-start px-4 py-3">
@@ -193,12 +177,8 @@ export default {
               </div>
               <div
                 class="col-1 d-flex align-items-center justify-content-evenly fs-4"
-                :key="dish.id"
-              >
-                <i
-                  class="bi bi-cart-dash"
-                  @click="decrementCounter(dish.id)"
-                ></i>
+                :key="dish.id">
+                <i class="bi bi-cart-dash" @click="decrementCounter(dish.id)"></i>
                 {{ getCartItemQuantity(dish.id) }}
                 <i class="bi bi-cart-plus" @click="incrementCounter(dish)"></i>
 
