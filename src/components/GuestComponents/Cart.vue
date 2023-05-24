256e535c338838fd72105fb823759966a241e9fc
Thomas-Basadonne <thommybasa@gmail.com>
2023-05-24 10:41:52 +0200

    add increment/decrement in cart

--- src/components/GuestComponents/Cart.vue
+++ src/components/GuestComponents/Cart.vue
@@ -60,6 +60,54 @@ export default {
       // console.log("index", index);
       return index;
     },
+    incrementCounter(dish) {
+      const cartItem = this.cartItems.find((item) => item.id === dish.id);
+      if (cartItem && cartItem.restaurant_id === dish.restaurant_id) {
+        cartItem.quantity++;
+      } else if (
+        this.cartItems.length == 0 ||
+        this.cartItems[0].restaurant_id == dish.restaurant_id
+      ) {
+        let obj = {
+          id: dish.id,
+          restaurant_id: dish.restaurant_id,
+          name: dish.name,
+          quantity: 1,
+          price: dish.price,
+          picture: dish.picture,
+        };
+        this.cartItems.push(obj);
+      } else {
+        alert(
+          "Questo piatto non può essere aggiunto al carrello perché è di un altro ristorante!"
+        );
+
+        document.getElementById("alert").innerHTML = `
+      <div class="alert alert-danger" role="alert">
+        Puoi ordinare da un ristorante alla volta, svuota il carrello per continuare!
+      </div>      
+      `;
+      }
+      this.sync(this.key, this.cartItems);
+    },
+
+    // riduce la quantity di un piatto nell' array cartItems e se la quantity=0 rimuove il piatto dall' array
+    decrementCounter(dishId) {
+      const cartItem = this.cartItems.find((item) => item.id === dishId);
+      if (cartItem && cartItem.quantity > 0) {
+        cartItem.quantity--;
+      }
+      if (cartItem && cartItem.quantity == 0) {
+        this.cartItems.splice(this.getIndexItem(dishId), 1);
+      }
+      this.sync(this.key, this.cartItems);
+    },
+
+    // indica la quantità di ogni singolo piatto all' interno dell' array cartItems, se il piatto non è presente nell' array imposta la quantity=0
+    getCartItemQuantity(dishId) {
+      const cartItem = this.cartItems.find((item) => item.id === dishId);
+      return cartItem ? cartItem.quantity : 0;
+    },
   },
 
   created() {},
@@ -79,7 +127,7 @@ export default {
             />
           </div>
           <div class="card-body row">
-            <div class="col">
+            <div class="col-6">
               <h5 class="card-title fw-bold">{{ cartItem.name }}</h5>
               <p class="card-text">Prezzo: {{ cartItem.price }}€</p>
             </div>
@@ -91,13 +139,27 @@ export default {
                 Prezzo totale: {{ singleDishTotalPrice(cartItem) }}€
               </p>
             </div>
+            <div class="col-3 d-flex flex-column align-items-center">
+              <div class="mb-2 fs-4">
+                <i
+                  class="bi bi-cart-dash"
+                  @click="decrementCounter(cartItem.id)"
+                ></i>
+                {{ getCartItemQuantity(cartItem.id) }}
+                <i
+                  class="bi bi-cart-plus"
+                  @click="incrementCounter(cartItem)"
+                ></i>
+              </div>
+
+              <button
+                class="btn custom-btn mx-3"
+                @click="removeDish(cartItem.id, cartItem.quantity)"
+              >
+                Rimuovi
+              </button>
+            </div>
           </div>
-          <button
-            class="btn custom-btn mx-3"
-            @click="removeDish(cartItem.id, cartItem.quantity)"
-          >
-            Rimuovi
-          </button>
         </div>
       </div>
     </div>
