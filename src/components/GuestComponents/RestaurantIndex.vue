497c955da1fdf682fef74a0f076245b7c7f8d617
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-05-27 15:02:11 +0200

    changed style of categories-container and card spacing

--- src/components/GuestComponents/RestaurantIndex.vue
+++ src/components/GuestComponents/RestaurantIndex.vue
@@ -64,9 +64,7 @@ export default {
     },
     toggleCheckbox(label) {
       if (this.selectedCategories.includes(label)) {
-        this.selectedCategories = this.selectedCategories.filter(
-          (category) => category !== label
-        );
+        this.selectedCategories = this.selectedCategories.filter((category) => category !== label);
       } else {
         this.selectedCategories.push(label);
       }
@@ -90,48 +88,36 @@ export default {
 
   <section class="bg mt-5 pb-5" style="margin-bottom: -1px">
     <div class="container py-5 text-center text-white">
-      <h1 class="mb-4 fw-bold">
-        Scopri i ristoranti più apprezzati della nostra città.
-      </h1>
+      <h1 class="mb-4 fw-bold">Scopri i ristoranti più apprezzati della nostra città.</h1>
       <h4>A casa tua a tempo <span style="color: #f8d24c"> di click!</span></h4>
     </div>
 
-    <div class="bg-category container shadow-sm">
-      <h4 class="fs-2 ms-3 mb-2" style="color: #bd2222">
-        Scegli cosa mangiare:
-      </h4>
-      <div
-        class="form-check form-check-inline my-2 mx-0"
-        v-for="category in categories"
-      >
-        <input
-          class="btn-check"
-          type="checkbox"
-          :value="category.label"
-          v-model="selectedCategories"
-          autocomplete="off"
-        />
-        <!-- forse  :for="category.id qui sotto -->
+    <div class="container">
+      <div class="bg-category shadow-sm">
+        <h4 class="fs-2 ms-3 mb-2" style="color: #bd2222">Scegli cosa mangiare:</h4>
+        <div class="form-check form-check-inline my-2 mx-0" v-for="category in categories">
+          <input
+            class="btn-check"
+            type="checkbox"
+            :value="category.label"
+            v-model="selectedCategories"
+            autocomplete="off" />
+          <!-- forse  :for="category.id qui sotto -->
 
-        <button
-          type="button"
-          class="btn category-btn m-0"
-          :class="{
-            customActive:
-              selectedCategories.includes(category.label) &&
-              checkboxChecked(category.label),
-          }"
-          data-bs-toggle="button"
-          :id="'toggleBtn-' + category.id"
-          @click="toggleCheckbox(category.label)"
-        >
-          <img
-            class="btn m-0 p-0 category-img"
-            :src="category.picture"
-            alt=""
-          />
-          <span class="btn-label">{{ category.label }}</span>
-        </button>
+          <button
+            type="button"
+            class="btn category-btn m-0"
+            :class="{
+              customActive:
+                selectedCategories.includes(category.label) && checkboxChecked(category.label),
+            }"
+            data-bs-toggle="button"
+            :id="'toggleBtn-' + category.id"
+            @click="toggleCheckbox(category.label)">
+            <img class="btn m-0 p-0 category-img" :src="category.picture" alt="" />
+            <span class="btn-label">{{ category.label }}</span>
+          </button>
+        </div>
       </div>
     </div>
   </section>
@@ -141,29 +127,23 @@ export default {
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 1440 100"
-    preserveAspectRatio="none"
-  >
+    preserveAspectRatio="none">
     <path
       class="wavePath-haxJK1 animationPaused-2hZ4IO"
       d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
-      fill="currentColor"
-    ></path>
+      fill="currentColor"></path>
   </svg>
 
   <!-- CARDS GRID -->
 
   <div class="container card-group pb-5">
-    <div class="row justify-content-center">
-      <div
-        class="col-lg-4 col-md-6 col-sm-12 g-5"
-        v-for="restaurant in filteredRestaurants"
-      >
+    <div class="row justify-content-between g-5">
+      <div class="col-lg-4 col-md-6 col-sm-12" v-for="restaurant in filteredRestaurants">
         <div class="card h-100 border shadow-lg">
           <img
             :src="restaurant.picture"
             class="card-img-top restaurant-picture img-fluid"
-            alt="Picture"
-          />
+            alt="Picture" />
           <div class="card-body d-flex flex-column justify-content-between">
             <h5 class="card-title text-center fw-bold mb-1">
               {{ restaurant.restaurant_name }}
@@ -174,9 +154,7 @@ export default {
             </h6>
 
             <div class="row justify-content-center">
-              <div
-                class="d-flex align-items-center justify-content-center flex-wrap"
-              >
+              <div class="d-flex align-items-center justify-content-center flex-wrap">
                 <p class="m-0 me-2">Cosa si mangia?</p>
                 <div v-for="category in restaurant.categories" class="">
                   <span class="card-text badge rounded-pill text-bg-danger">{{
@@ -193,8 +171,7 @@ export default {
                   id: restaurant.id,
                 },
               }"
-              class="nav-link d-flex justify-content-center mt-1"
-            >
+              class="nav-link d-flex justify-content-center mt-1">
               <button class="btn custom-btn">Vai al Menu</button>
             </router-link>
           </div>
